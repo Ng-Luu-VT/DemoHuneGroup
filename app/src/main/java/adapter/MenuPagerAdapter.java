f1735eecb2933bf38203703eb303103ed09b1830
toan.nguyen3 <toan.nguyen3@mservice.com.vn>
2020-07-31 14:59:35 +0700

    add menu

---
+++ app/src/main/java/adapter/MenuPagerAdapter.java
@@ -0,0 +1,11 @@
+package adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MenuPagerAdapter {
+
+
+}
